49298d1e59b21178b0bd257f401d920b976b1475
dornelasxz <rafaeldornelasalmeida@gmail.com>
2024-11-22 03:00:57 -0300

    aula_4

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -4,11 +4,17 @@ import conectarAoBanco from "../config/dbConfig.js";
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // Função assíncrona que recupera todos os posts armazenados no banco de dados.
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
     // Obtém uma referência ao banco de dados chamado "imersao-instabytes".
     const db = conexao.db("imersao-instabytes");
     // Obtém uma referência à coleção chamada "posts".
     const colecao = db.collection("posts");
     // Recupera todos os documentos da coleção como um array.
     return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost)
 }
